4723351add9b259504bb9bb557617a7ef2c3cb99
Shamim Hossen <shamim41.cse@gmail.com>
2023-12-04 13:31:55 +0600

    resole error

--- scanner.py
+++ scanner.py
@@ -10,3 +10,26 @@ if len(sys.argv) == 2:
 else:
     print("Invalid amount of arguments")
     print("Syntax: python3 scanner.py <ip address>")
+# create a banner
+
+print("-" * 50)
+print("Scanning running: " + target)
+print("Time started: " + str(datetime.now()))
+print("-" * 50)
+try:
+    for port in range(50, 85):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket.setdefaulttimeout(1)
+        result = s.connect_ex((target, port))
+        if result == 0:
+            print(f"Port {port} " is open)
+        s.close()
+
+except KeyboardInterrupt:
+    print("\nExiting")
+    sys.exit()
+except socket.gaierror:
+    print("Hostname could not be resolved.")
+    sys.exit()
+except socket.error:
+    print("Could not connect.")
